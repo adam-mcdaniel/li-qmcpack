1110a109b550e384af168d96b08627510da8a0f2
Adam McDaniel <adam.mcdaniel17@gmail.com>
2026-01-29 14:39:44 -0500

    Fixed issue with using wrong libamdhip64.so

--- compile.sh
+++ compile.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-module purge
+QMCPACK_ROOT=$(pwd)
 
+module reset
+
+# Load gpu offload for gfx90a
 source ../setup-build-env.sh
-export QMCPACK_ROOT=$(pwd)
-export PREFIX=$QMCPACK_ROOT/install
-rm -rf $QMCPACK_ROOT/build $QMCPACK_ROOT/CMakeCache.txt $QMCPACK_ROOT/CMakeFiles
 
-export CFLAGS="-fopenmp"
-export CXXFLAGS="-fopenmp"
+module load PrgEnv-cray
+module load amd/6.4.1
+module load rocm/6.4.1
+module load craype-accel-amd-gfx90a
+module load cray-hdf5
+module load cray-fftw
+module load openblas
+module load cray-mpich/8.1.31
 
-mkdir build && cd build
+module list
+
+echo "ROCM PATH: $ROCM_PATH"
+echo "OPENBLAS ROOT: $OLCF_OPENBLAS_ROOT"
+
+rm -Rf build && mkdir build && cd build
+
+export CFLAGS="-fopenmp -L$CRAY_MPICH_PREFIX/lib/ -lmpi_gtl_hsa"
+export CXXFLAGS="-fopenmp -L$CRAY_MPICH_PREFIX/lib/ -lmpi_gtl_hsa"
+
 cmake -DCMAKE_BUILD_TYPE=Release \
+    -DBLAS_LIBRARIES="$OLCF_OPENBLAS_ROOT/lib/libopenblas.so" \
+    -DLAPACK_LIBRARIES="$OLCF_OPENBLAS_ROOT/lib/libopenblas.so" \
     -DQMC_GPU="hip" \
     -DQMC_GPU_ARCHS="gfx90a" \
     -DQMC_MPI="on" \
--- run.sh
+++ run.sh
@@ -3,9 +3,21 @@
 QMCPACK_ROOT=$(pwd)
 
 source ../setup-run-env.sh
+
+module load PrgEnv-cray
+module load amd/6.4.1
+module load rocm/6.4.1
+module load craype-accel-amd-gfx90a
+module load cray-hdf5
+module load cray-fftw
+module load openblas
+module load cray-mpich/8.1.31
+
 if [ -f ../profiler-injector.sh ]; then
     source ../profiler-injector.sh
 fi
+
+ldd $QMCPACK_ROOT/build/bin/qmcpack
 
 export OMP_NUM_THREADS=4
 srun -A csc688 -t10 -N1 -c7 -n8 --gpu-bind=closest $QMCPACK_ROOT/build/bin/qmcpack /lustre/orion/csc688/world-shared/interceptor-bench/li-qmcpack/examples/molecules/He/he_simple.xml
